ef2ef9eadd1067db5986eff290ba0c26d46bc688
MiguelViapiana <migvjung@gmail.com>
2024-04-15 11:08:37 -0300

    Programa adicionado

--- Projeto2/src/br/edu/up/Program.java
+++ Projeto2/src/br/edu/up/Program.java
@@ -1,6 +1,15 @@
 package br.edu.up;
+import br.edu.up.controllers.*;
+import br.edu.up.models.*;
+import br.edu.up.views.*;
+
 public class Program {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Ponto ponto1 = new Ponto();
+        Ponto ponto2 = new Ponto(2.0, 5.0);
+        PontoView view = new PontoView();
+        PontoController controller = new PontoController(ponto1, ponto2, view);
+        
+        
     }
 }
