27d9b43514174a72b5da9b9ffa7b6a7450c9a9ab
ArnaldoSJunior <arnaldojuniorads@gmail.com>
2024-04-15 10:35:37 -0300

    p2

--- Projeto2/src/br/edu/up/models/Ponto.java
+++ Projeto2/src/br/edu/up/models/Ponto.java
@@ -2,4 +2,33 @@ package br.edu.up.models;
 
 public class Ponto {
 
+ private Double x;
+ private Double y;
+
+
+public Ponto(Double x, Double y) {
+    this.x = x;
+    this.y = y;
+}
+
+public Ponto() {
+    this.x = 0.0;
+    this.y = 0.0;
+}
+
+public Double getX() {
+    return x;
+}
+public void setX(Double x) {
+    this.x = x;
+}
+public Double getY() {
+    return y;
+}
+public void setY(Double y) {
+    this.y = y;
+}
+
+ 
+
 }
