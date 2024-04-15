42e819d17939ebe6b7422f4a5e8cbc4405268801
ArnaldoSJunior <arnaldojuniorads@gmail.com>
2024-04-15 09:43:55 -0300

    segundo

--- Projeto1/src/br/edu/up/controls/LivroController.java
+++ Projeto1/src/br/edu/up/controls/LivroController.java
@@ -1,5 +1,15 @@
 package br.edu.up.controls;
 
 public class LivroController {
+    private Livro model;
+    private LivroView view;
 
+    public LivroController(Livro model, LivroView view){
+        this.model = model;
+        this.view = view;
+    }
+
+    public void atualizarView(){
+        view.exebirLivro(model.obterDadosFormatados(),"Livro");
+    }
 }
--- Projeto1/src/br/edu/up/models/Livro.java
+++ Projeto1/src/br/edu/up/models/Livro.java
@@ -2,4 +2,69 @@ package br.edu.up.models;
 
 public class Livro {
 
+    private String codigo;
+    private String titulo;
+    private String autores;
+    private String isbn;
+    private int ano;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutores() {
+        return autores;
+    }
+
+    public void setAutores(String autores) {
+        this.autores = autores;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public Livro() {
+    }
+
+    public Livro(String codigo, String titulo, String autores, String isbn, int ano) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.autores = autores;
+        this.isbn = isbn;
+        this.ano = ano;
+    }
+
+    public String obterDadosFormatados() {
+        return "Código: " + codigo + "\n" +
+                "Título: " + titulo + "\n" +
+                "Autores: " + autores + "\n" +
+                "ISBN" + isbn + "\n" +
+                "Ano: " + ano;
+    }
+
 }
