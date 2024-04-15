88c121e0fcde394966986c5aaebf51377deed433
MiguelViapiana <migvjung@gmail.com>
2024-04-15 09:42:51 -0300

    View criada

--- Projeto1/src/br/edu/up/view/LivroView.java
+++ Projeto1/src/br/edu/up/view/LivroView.java
@@ -1,5 +1,9 @@
 package br.edu.up.view;
 
 public class LivroView {
-
+    public void exebirLivro(String dadosLivro, String tituloLivro){
+        System.out.println(tituloLivro+ ":");
+        System.out.println(dadosLivro);
+        System.out.println();
+    }
 }
