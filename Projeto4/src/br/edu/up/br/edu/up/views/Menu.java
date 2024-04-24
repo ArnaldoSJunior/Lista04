6c02bf9f3c19c376913ad926aae230e6a89fe6f6
ArnaldoSJunior <arnaldojuniorads@gmail.com>
2024-04-24 18:44:28 -0300

    menu estruturado

--- Projeto4/src/br/edu/up/br/edu/up/views/Menu.java
+++ Projeto4/src/br/edu/up/br/edu/up/views/Menu.java
@@ -1,6 +1,7 @@
 package br.edu.up.views;
 
 import br.edu.up.Prompt;
+import br.edu.up.models.Carro;
 import br.edu.up.models.Estacionamento;
 public class Menu {
    
@@ -22,31 +23,62 @@ public class Menu {
         System.out.println();
     
         int opcao = Prompt.lerInteiro();
+        do {
         switch (opcao) {
             case 1:{
-                    mostrarVagasDisponiveis();
+                mostrarVagasDisponiveis();
                 }
                  break;
             case 2:{
-    
+                Prompt.lerDecimal("Digite o modelo: ");
+                Prompt.lerDecimal("Digite a placa : ");
+                Prompt.lerDecimal("Digite a cor: ");
                 }
                  break;
             case 3:{
-    
+                Prompt.lerDecimal("Digite a placa : ");
             }break;
             case 4:{
+                System.out.println("Escolha o período: ");
+                System.out.println("1. Manhã");
+                System.out.println("2. Tarde");
+                System.out.println("3. Noite");
+                System.out.println("4. Retornar ao Menu principal");
+
+                int emitir = Prompt.lerInteiro();
+                    switch (emitir) {
+                        case 1:
+                        
+                            break;
+                        case 2:
+                        
+                             break;
+                        case 3:
+                        
+                             break;
+                        case 4:{
+                         mostrar();
+                        }
+                            break;
+                
+                    default:
+                        break;
+                }
     
             }break;
             default:
                 break;
         }
-    
+    } while (opcao != 5);
     }
      public void mostrarVagasDisponiveis(){
-        Estacionamento novo = new Estacionamento();
+       Estacionamento novo = new Estacionamento();
        novo.vagasDisponiveis();
        System.out.println("Vagas: " + novo.vagasDisponiveis());
      }
+
+     
+     
 }
 
 
