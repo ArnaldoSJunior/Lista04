d766e69688555fd78616e7bb562234a4f3894bcb
ArnaldoSJunior <arnaldojuniorads@gmail.com>
2024-05-17 10:32:14 -0300

    menu

---
+++ Projeto8/src/br/edu/up/views/Menu.java
@@ -0,0 +1,48 @@
+package br.edu.up.views;
+
+import br.edu.up.Prompt;
+
+public class Menu {
+
+    public void mostrar() {
+
+        System.out.println("================Menu================");
+        System.out.println();
+        System.out.println("1.Incluir contato pessoal");
+        System.out.println("2.Incluir contato comercial");
+        System.out.println("3.Excluir um contato pelo código");
+        System.out.println("4.Consultar um contato pelo código");
+        System.out.println("5.Listar todos os contatos");
+        System.out.println("6.Sair do programa ");
+        System.out.println();
+
+        int op = Prompt.lerInteiro();
+
+        do {
+            switch (op) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                default:
+                    break;
+            }
+        } while (op != 6);
+
+    }
+
+}
